23c20719da86e94e372e389683155572d5af8424
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2021-10-23 17:55:13 +0530

    Initial commit
    Defined "void emailAlerter(void)" and "void ledAlerter(void)" functions

---
+++ alerter.c
@@ -0,0 +1,13 @@
+#include "stats.h"
+
+//Function to capture the number of times email alert has been received
+void emailAlerter(void)
+{
+  emailAlertCallCount = emailAlertCallCount + 1;
+}
+
+//Function to capture the number of times LED alert has been received
+void ledAlerter(void)
+{
+  ledAlertCallCount = ledAlertCallCount + 1;
+}
