03f0b2589883241ac2dd5b64a33a64c5faabdefa
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2021-10-23 18:01:33 +0530

    Added check_and_alert function
    Added check_and_alert function

--- stats.c
+++ stats.c
@@ -1,5 +1,10 @@
 #include "stats.h"
 
+//Global variables shared by other .c files
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+// Function to compute min, max and average from the input array of values
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     float indexedArrayElement,sum;
@@ -8,25 +13,37 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     sum = 0;
     arrayIndex = 0;
     s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.min = s.max = numberset[0];
     
     for(arrayIndex=0; arrayIndex<setlength; arrayIndex++)
     {
-         if(s.min>numberset[arrayIndex])
-         {
-		    s.min=numberset[arrayIndex]; 
-         }
-		 if(s.max<numberset[arrayIndex])
-         {
-		    s.max=numberset[arrayIndex];
-         }
+        if(s.min>numberset[arrayIndex])
+        {
+	    s.min=numberset[arrayIndex]; 
+        }
+	if(s.max<numberset[arrayIndex])
+        {
+	    s.max=numberset[arrayIndex];
+        }
+	// Sum to calculate average subsequently 
         sum = (sum + numberset[arrayIndex]);
     }
     s.average = (sum / setlength);
-    return s;
-    
+    // Return the structure with the computed min, max and average values
+    return s;    
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+// Function to check and raise alert based on the computed value and the threshold limit
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    // If computedStats.max breaches the maxThreshold value, then raise alert !
+    if(computedStats.max > maxThreshold)
+    {
+	// Raise email alert
+        alerters[0]();
+	// Raise LED alert
+        alerters[1]();
+    }	
+}
+	
+
