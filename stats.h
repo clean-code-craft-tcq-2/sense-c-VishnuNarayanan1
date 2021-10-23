e14e4775040981c99e3a77ad000e7167311643e9
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2021-10-23 16:53:33 +0530

    Initial commit with build error resolved
    Added structure "Stats" declaration

--- stats.h
+++ stats.h
@@ -1,4 +1,11 @@
 
+struct Stats
+{
+    float average;
+    float min;
+    float max;
+};
+
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
@@ -6,3 +13,4 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+
