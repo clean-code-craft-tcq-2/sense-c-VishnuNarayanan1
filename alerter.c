b82f466e5ef23217219ffae45a705ee67981d20a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2021-10-23 19:19:14 +0530

    Updated indentation

--- alerter.c
+++ alerter.c
@@ -4,11 +4,11 @@
 //Function to capture the number of times email alert has been received
 void emailAlerter(void)
 {
-  emailAlertCallCount = emailAlertCallCount + 1;
+    emailAlertCallCount = emailAlertCallCount + 1;
 }
 
 //Function to capture the number of times LED alert has been received
 void ledAlerter(void)
 {
-  ledAlertCallCount = ledAlertCallCount + 1;
+    ledAlertCallCount = ledAlertCallCount + 1;
 }
