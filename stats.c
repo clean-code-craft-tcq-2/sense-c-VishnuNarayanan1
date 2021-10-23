610e3c8c6a4cc3bea86a427930809270db5c551a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2021-10-23 16:52:13 +0530

    Initial commit with few build errors resolved
    Initial commit with function return and min,max and average computation

--- stats.c
+++ stats.c
@@ -2,9 +2,30 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
+    float indexedArrayElement,sum;
+    int arrayIndex;
+    indexedArrayElement = 0;
+    sum = 0;
+    arrayIndex = 0;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    for(arrayIndex=0; arrayIndex<setlength; arrayIndex++)
+    {
+         if(s.min>numberset[arrayIndex])
+         {
+		    s.min=numberset[arrayIndex]; 
+         }
+		 if(s.max<numberset[arrayIndex])
+         {
+		    s.max=numberset[arrayIndex];
+         }
+        sum = (sum + numberset[arrayIndex]);
+    }
+    s.average = (sum / setlength);
+    return s;
+    
 }
 
 int emailAlertCallCount = 0;
